a2d89ced687645a50c0976ace6406380e5f6db1f
Denis Renam <denis.ursulino@gmail.com>
2015-10-18 13:52:26 -0200

    5new

--- Practicar.Nimble.IProject/bld/Windows-AnyCPU/Debug/www/scripts/Controllers.js
+++ Practicar.Nimble.IProject/bld/Windows-AnyCPU/Debug/www/scripts/Controllers.js
@@ -10,34 +10,12 @@ nimbleControllers.controller('ControllerInicial', ['$scope', '$http',
           { 'url': '#Sobre', 'text': 'Sobre Nimble' },
       ];
 
-
-
-
-
-      $(".tile").height($("#tile1").width());
-      $(".carousel").height($("#tile1").width());
-      $(".item").height($("#tile1").width());
-
-      $(window).resize(function () {
-          if (this.resizeTO) clearTimeout(this.resizeTO);
-          this.resizeTO = setTimeout(function () {
-              $(this).trigger('resizeEnd');
-          }, 10);
-      });
-
-      $(window).bind('resizeEnd', function () {
-          $(".tile").height($("#tile1").width());
-          $(".carousel").height($("#tile1").width());
-          $(".item").height($("#tile1").width());
-      });
-
-
       $http.get(
             'http://localhost:15117/Methods.asmx/HelloWorld')
           .success(function (data) {
               console.log(data);
               $scope.retorno = data.text;
-          }
+        }
         )
 
       //$scope.retorno = ServiceHelloWorld.GetMsg("Exemplo de Entrada");
